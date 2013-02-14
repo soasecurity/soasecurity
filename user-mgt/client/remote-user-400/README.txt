bb5c0b195ea0286fc365a96a78a6e76ccaa86e4f
Asela <asela@asela-laptop.(none)>
2013-02-14 11:58:51 +0530

    add

--- user-mgt/client/remote-user-400/README.txt
+++ user-mgt/client/remote-user-400/README.txt
@@ -1,31 +1,36 @@
-This contains a sample client to demo the remote user management service that expose carbon the user store manager 
-as a web service. You can use this web service for authentication and do user and role management functions.
-This project contains a web service client source can be used to call this web service.  
-Also basically you can use this code as bridge for your own application and the web service.
+This project contains a sample client to demo the user management service that is exposed by carbon.
+There are two web services that can be used  for these function. One is UserAdmin and other one is
+RemoteUserStoreManagerService (which is only available with WSO2 IS). UserAdmin web service API
+has been written thinking of Carbon user management UI. Therefore the best API is the RemoteUserStoreManagerService
+that can be used for external application, such as web applications.
+You can use this web service for authentication and do user and role management functions.
+This project contains a web service client source can be used to call this web service.
+Basically  you can use this code as bridge for your own application and the web service.
 
 How to Run Sample
 -----------------
 
 1. Build project using maven3
-2. Run the sample
-
-Or else
-
-1. Start WSO2 Carbon Server
-2. Run ant command in <CARBON_HOME>/bin/ . This will copy required jars into <CARBON_HOME>/repository/lib directory
+2. Start WSO2 Carbon Server
 3. Modify the parameters which are defined in "SampleUserRoleMgtClient" class according your configuration. (More details would be available in code itself)
-4. Add jars in following directory into project's class path
+4. Run the sample
+
+Or else you can run this without building using maven
+
+1. Run ant command in <CARBON_HOME>/bin/ . This will copy required jars into <CARBON_HOME>/repository/lib directory
+2. Add jars in following directory into project's class path
         <CARBON_HOME>/repository/lib
-5. Run "SampleUserRoleMgtClient" client
 
 
-Note: you can see the WSDL of RemoteUserManagementService Service
+Note: You can see the WSDL of RemoteUserStoreManagerService by going through following steps
 
-1. UserAdmin is admin service according the WSO2 Carbon platform. Therefore WSDL of admin service can not be seen by default.
+1. RemoteUserStoreManagerService is admin service according the WSO2 Carbon platform.
+     Therefore WSDL of admin service can not be seen by default.
 2. Open carbon.xml file which can be found at <IS_HOME>/repository/conf directory
 3. Locate following XML element and set it to “false”
 
  <HideAdminServiceWSDLs>true</HideAdminServiceWSDLs>
 
 4. Then restart WSO2 Identity Server running wso2server script which can be found at <IS_HOME>/bin directory.
-5. Point to browser https://localhost:9443/services/RemoteUserStoreManagerService?wsdl If you have started server in default configurations.
+5. Point to browser https://localhost:9443/services/RemoteUserStoreManagerService?wsdl
+     If you have started server in default configurations.
--- user-mgt/client/remote-user-400/src/main/java/org/soa/security/sample/user/mgt/SampleUserRoleMgtClient.java
+++ user-mgt/client/remote-user-400/src/main/java/org/soa/security/sample/user/mgt/SampleUserRoleMgtClient.java
@@ -1,22 +1,20 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ *  Copyright 2013 soasecurity.org
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
-package org.wso2.carbon.user.mgt.sample;
+package org.soa.security.sample.user.mgt;
 
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
@@ -24,7 +22,7 @@ import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
 import org.apache.axis2.transport.http.HTTPConstants;
 import org.apache.axis2.transport.http.HttpTransportProperties;
-import org.wso2.carbon.user.mgt.stub.UserAdminStub;
+import org.wso2.carbon.um.ws.api.stub.RemoteUserStoreManagerServiceStub;
 
 import java.io.File;
 
@@ -57,7 +55,7 @@ public class SampleUserRoleMgtClient {
 
 
         /**
-         * trust store path.  this must contains server's certificate. 
+         * trust store path.  this must contains server's  certificate or Server's CA chain 
          */
         String trustStore = System.getProperty("user.dir") + File.separator +
                                                          "src" + File.separator + "main" + File.separator +
@@ -65,9 +63,9 @@ public class SampleUserRoleMgtClient {
 
         /**
          * Call to https://localhost:9443/services/   uses HTTPS protocol.
-         * Therefore we to validate the server certificate. The server certificate is looked up in the
-         * trust store. Following code sets what trust-store to look for and its JKs password.
-         * Note : The trust store should have server's certificate.
+         * Therefore we to validate the server certificate or CA chain. The server certificate is looked up in the
+         * trust store.
+         * Following code sets what trust-store to look for and its JKs password.
          */
 
         System.setProperty("javax.net.ssl.trustStore",  trustStore );
@@ -83,34 +81,47 @@ public class SampleUserRoleMgtClient {
 
             /**
              * Create a configuration context. A configuration context contains information for
-             * axis2 environment. This is needed to create an axis2 client
+             * axis2 environment. This is needed to create an axis2 service client
              */
             configContext = ConfigurationContextFactory.createConfigurationContextFromFileSystem( null, null);
 
+            /**
+             * end point url with service name
+             */
             String serviceEndPoint = SEVER_URL + "UserAdmin";
 
-            UserAdminStub adminStub = new UserAdminStub(configContext, serviceEndPoint);
+            /**
+             * create stub and service client
+             */
+            RemoteUserStoreManagerServiceStub adminStub = new RemoteUserStoreManagerServiceStub(configContext, serviceEndPoint);
             ServiceClient client = adminStub._getServiceClient();
             Options option = client.getOptions();
 
             /**
-             * setting basic auth headers for authentication for user admin 
+             * Setting a authenticated cookie that is received from Carbon server.
+             * If you have authenticated with Carbon server earlier, you can use that cookie, if
+             * it has not been expired
+             */
+            option.setProperty(HTTPConstants.COOKIE_STRING, null);
+
+            /**
+             * Setting basic auth headers for authentication for carbon server
              */
             HttpTransportProperties.Authenticator auth = new HttpTransportProperties.Authenticator();
-            option.setProperty(HTTPConstants.COOKIE_STRING, null);
             auth.setUsername(USER_NAME);
             auth.setPassword(PASSWORD);
             auth.setPreemptiveAuthentication(true);
             option.setProperty(org.apache.axis2.transport.http.HTTPConstants.AUTHENTICATE, auth);
             option.setManageSession(true);
 
+
             /**
-             * Do any thing with user admin API.  you can list user. add users, roles, assignments...
+             * Do any thing with user mgt API.  you can authenticate users, list user. add users, roles, assignments...
              * Here as an example just have implemented list user operation.
              */
 
+            String[] users = adminStub.listUsers("*", 100);
 
-            String[] users = adminStub.listUsers("*");
             if(users != null){
                 System.out.println("Listing user names of Carbon server...... ");
                 for(String user : users){
@@ -118,10 +129,44 @@ public class SampleUserRoleMgtClient {
                 }
             }
 
+            /**
+             * creates user
+             */
+
+            try{
+                adminStub.addUser("asela", "password", null, null, null, false);
+                System.out.println("User is created successfully");
+            } catch (Exception e){
+                System.err.println("User creation is failed");
+                e.printStackTrace();
+            }
+
+            /**
+             * authenticate User
+             */
+            boolean  authenticate = false;
+
+            try{
+                authenticate = adminStub.authenticate("asela", "password");
+            } catch (Exception e ){
+                e.printStackTrace();
+            }
+
+            if(authenticate){
+                System.out.println("User is authenticated successfully");
+            } else {
+                System.err.println("User is authentication failed");        
+            }
+
+
+            /**
+             * you can retrieve the cookie to use for sub sequent communications
+             */
             String authCookie = (String) adminStub._getServiceClient().getServiceContext()
                     .getProperty(HTTPConstants.COOKIE_STRING);
 
             System.out.println(authCookie);
+                                    
             /**
              * If WSO2 Carbon has been connected with multiple user stores. Say you need to create a user in
              * domain called  it.com,  user name must be passed as   it.com/asela
